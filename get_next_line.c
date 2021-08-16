ccc50ccd831129e7bd9275a8a7ad619dabe692d3
ymori <yuu.mori06@gmail.com>
2021-08-16 08:31:41 +0200

    clean: refactor get_next_line
    Fixing typos
    for norm v3

--- get_next_line.c
+++ get_next_line.c
@@ -24,7 +24,7 @@ static int	set_up_line(ssize_t read_size, char **loaded_str, \
 		tmp = ft_strdup(&loaded_str[fd][i + 1]);
 		free(loaded_str[fd]);
 		loaded_str[fd] = tmp;
-		if (loaded_str[fd][0] == '0')
+		if (loaded_str[fd][0] == '\0')
 			str_free(&loaded_str[fd]);
 	}
 	else
@@ -51,28 +51,33 @@ static int	end_process(ssize_t read_size, char **loaded_str, \
 		return (set_up_line(read_size, loaded_str, line, fd));
 }
 
+static void	update_loaded_str(char **loaded_str, char *buf_str)
+{
+	char	*tmp;
+
+	tmp = ft_strjoin(*loaded_str, buf_str);
+	free(*loaded_str);
+	*loaded_str = tmp;
+}
+
 int	get_next_line(int fd, char **line)
 {
 	static char	*loaded_str[OPEN_MAX];
 	char		*buf_str;
-	char		*tmp;
 	ssize_t		read_size;
 
-	if (fd < 0 || fd >= OPEN_MAX)
+	buf_str = malloc(((size_t)BUFFER_SIZE + 1) * sizeof(char));
+	if (fd < 0 || fd >= OPEN_MAX || !line || !buf_str)
 		return (-1);
-	if (!line || !(buf_str = malloc(((size_t)BUFFER_SIZE + 1) * sizeof(char))))
-		return (-1);
-	while ((read_size = read(fd, buf_str, BUFFER_SIZE)) > 0)
+	read_size = read(fd, buf_str, BUFFER_SIZE);
+	while (read_size > 0)
 	{
 		buf_str[read_size] = '\0';
 		if (loaded_str[fd] == NULL)
 			loaded_str[fd] = ft_strdup(buf_str);
 		else
-		{
-			tmp = ft_strjoin(loaded_str[fd], buf_str);
-			free(loaded_str[fd]);
-			loaded_str[fd] = tmp;
-		}
+			update_loaded_str(&loaded_str[fd], buf_str);
+		read_size = read(fd, buf_str, BUFFER_SIZE);
 		if (ft_memchr(loaded_str[fd], '\n', read_size))
 			break ;
 	}
