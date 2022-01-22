1b75a5eb11e271d14491e0e61ff758a8afbe79ee
SoichiroSugimoto <fcsoichi1998@gmail.com>
2022-01-22 14:10:14 +0900

    split exe dir

--- srcs/exe/execute.c
+++ srcs/exe/execute.c
@@ -6,7 +6,7 @@
 /*   By: sosugimo <sosugimo@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 16:49:07 by sosugimo          #+#    #+#             */
-/*   Updated: 2022/01/21 17:00:00 by sosugimo         ###   ########.fr       */
+/*   Updated: 2022/01/22 14:08:19 by sosugimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,62 +15,6 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdio.h>
-
-void	execute_simple_command(t_astree *simple_cmd_node,
-							 bool async,
-							 bool stdin_pipe,
-							 bool stdout_pipe,
-							 int pipe_read,
-							 int pipe_write,
-							 char *redirect_in,
-							 char *redirect_out
-								)
-{
-	CommandInternal cmdinternal;
-	init_command_internal(simple_cmd_node, &cmdinternal, async, stdin_pipe, stdout_pipe,
-						  pipe_read, pipe_write, redirect_in, redirect_out
-						 );
-	execute_command_internal(&cmdinternal);
-	destroy_command_internal(&cmdinternal);
-}
-
-void	execute_command(t_astree *cmdNode,
-							bool async,
-							bool stdin_pipe,
-							bool stdout_pipe,
-							int pipe_read,
-							int pipe_write)
-{
-	if (cmdNode == NULL)
-		return ;
-	if (cmdNode->type == NODE_REDIRECT_IN)
-		execute_simple_command(cmdNode->right,
-								async,
-								stdin_pipe,
-								stdout_pipe,
-								pipe_read,
-								pipe_write,
-								cmdNode->szData, NULL
-							  );
-	if (cmdNode->type == NODE_REDIRECT_OUT)
-		execute_simple_command(cmdNode->right,
-								async,
-								stdin_pipe,
-								stdout_pipe,
-								pipe_read,
-								pipe_write,
-								NULL, cmdNode->szData
-							  );
-	if (cmdNode->type == NODE_CMDPATH)
-		execute_simple_command(cmdNode,
-								async,
-								stdin_pipe,
-								stdout_pipe,
-								pipe_read,
-								pipe_write,
-								NULL, NULL
-							  );
-}
 
 void	execute_pipeline(t_astree *t, bool async)
 {
