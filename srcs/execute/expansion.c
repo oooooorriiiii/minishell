7e074be82d4621a5cf045723c1f89336a639afe4
SoichiroSugimoto <fcsoichi1998@gmail.com>
2022-01-28 17:27:31 +0900

    202201128

--- srcs/execute/expansion.c
+++ srcs/execute/expansion.c
@@ -6,7 +6,7 @@
 /*   By: sosugimo <sosugimo@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 17:07:09 by sosugimo          #+#    #+#             */
-/*   Updated: 2022/01/27 21:48:36 by sosugimo         ###   ########.fr       */
+/*   Updated: 2022/01/28 17:26:15 by sosugimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,41 @@
 # define single_q 1
 # define double_q 10
 
-char	*expand_enval(char *str)
+/*
+ssize_t	expand_enval(char **dst, char *src)
 {
 	char	**split;
 	char	*enval;
 	char	*tmp;
+	int		i;
+
+	i = 1;
+	split = ft_split(src, '$');
+	*dst = getenv(split[0]);
+	if (*dst == NULL)
+		*dst = split[0];
+	while (split[i])
+	{
+		enval = getenv(split[i]);
+		if (enval != NULL)
+			ft_strlcat(*dst, enval, strlen(*dst) + strlen(enval) + 1);
+		i++;
+	}
+	return (ft_strlen(*dst));
+}
+
+int	expand_enval_cnt(char *str)
+{
+	char	**split;
+	char	*enval;
 	char	*res;
 	int		i;
 
 	i = 1;
+	res = NULL;
 	split = ft_split(str, '$');
-	res = getenv(split[0]);
+	if (str[0] == '$')
+		res = getenv(split[0]);
 	if (res == NULL)
 		res = split[0];
 	while (split[i])
@@ -35,8 +59,9 @@ char	*expand_enval(char *str)
 			ft_strlcat(res, enval, strlen(res) + strlen(enval) + 1);
 		i++;
 	}
-	return (res);
+	return (ft_strlen(str) - (int)ft_strlen(res));
 }
+*/
 
 int	quote_skip_strlen(char *arguments, int *quote, int *param)
 {
@@ -47,12 +72,12 @@ int	quote_skip_strlen(char *arguments, int *quote, int *param)
 	res = 0;
 	while (arguments[i])
 	{
-		if (*arguments[i] == '\'' && (!*quote || *quote == single_q))
+		if (arguments[i] == '\'' && (!*quote || *quote == single_q))
 		{
 			*quote += single_q;
 			*param = single_q;
 		}
-		else if (*arguments[i] == '\"' && (!*quote || *quote == double_q))
+		else if (arguments[i] == '\"' && (!*quote || *quote == double_q))
 		{
 			*quote += double_q;
 			*param = double_q;
@@ -68,8 +93,9 @@ int	quote_skip_strlen(char *arguments, int *quote, int *param)
 
 void	quote_skip_strcpy(char *dst, char *src, int quote)
 {
-	int	i;
-	int	skip;
+	int		i;
+	int		skip;
+	char	*expand;
 
 	i = 0;
 	skip = 0;
@@ -94,12 +120,48 @@ void	quote_skip_strcpy(char *dst, char *src, int quote)
 	}
 }
 
-void	extra_strcpy(t_cmd_args *args, t_astree *ast_node)
+int	isenval(char *str)
 {
 	int	i;
-	int	quote;
-	int	param;
-	int	len;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+ssize_t	expand_enval(char **dst, char *src)
+{
+	char	**split;
+	char	*enval;
+	char	*tmp;
+	int		i;
+
+	i = 1;
+	split = ft_split(src, '$');
+	*dst = getenv(split[0]);
+	if (*dst == NULL)
+		*dst = split[0];
+	while (split[i])
+	{
+		enval = getenv(split[i]);
+		if (enval != NULL)
+			ft_strlcat(*dst, enval, strlen(*dst) + strlen(enval) + 1);
+		i++;
+	}
+	return (ft_strlen(*dst));
+}
+
+void	extra_strcpy(t_cmd_args *args, t_astree *ast_node)
+{
+	int			i;
+	int			quote;
+	int			param;
+	int			len;
 
 	i = 0;
 	quote = 0;
@@ -107,10 +169,14 @@ void	extra_strcpy(t_cmd_args *args, t_astree *ast_node)
 			|| ast_node->type == NODE_CMDPATH))
 	{
 		len = quote_skip_strlen(ast_node->szData, &quote, &param);
+		/////////////// 環境変数を展開する
+		if (isenval(ast_node->szData))
+		{
+			expand_enval(&(ast_node->szData), ast_node->szData);
+			len = quote_skip_strlen(ast_node->szData, &quote, &param);
+		}
 		args->cmdpath[i] = (char *)malloc(len + 1);
 		quote_skip_strcpy(args->cmdpath[i], ast_node->szData, param);
-		if (quote != single_q)
-			expand_enval(args->cmdpath[i]);
 		ast_node = ast_node->right;
 		i++;
 	}
