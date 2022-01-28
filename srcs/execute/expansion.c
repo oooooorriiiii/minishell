95008cb5bf0f6752284e374196b89ed65aae5494
SoichiroSugimoto <fcsoichi1998@gmail.com>
2022-01-28 17:42:56 +0900

    202201128-2

--- srcs/execute/expansion.c
+++ srcs/execute/expansion.c
@@ -6,7 +6,7 @@
 /*   By: sosugimo <sosugimo@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 17:07:09 by sosugimo          #+#    #+#             */
-/*   Updated: 2022/01/28 17:26:15 by sosugimo         ###   ########.fr       */
+/*   Updated: 2022/01/28 17:42:05 by sosugimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,6 @@
 
 # define single_q 1
 # define double_q 10
-
-/*
-ssize_t	expand_enval(char **dst, char *src)
-{
-	char	**split;
-	char	*enval;
-	char	*tmp;
-	int		i;
-
-	i = 1;
-	split = ft_split(src, '$');
-	*dst = getenv(split[0]);
-	if (*dst == NULL)
-		*dst = split[0];
-	while (split[i])
-	{
-		enval = getenv(split[i]);
-		if (enval != NULL)
-			ft_strlcat(*dst, enval, strlen(*dst) + strlen(enval) + 1);
-		i++;
-	}
-	return (ft_strlen(*dst));
-}
-
-int	expand_enval_cnt(char *str)
-{
-	char	**split;
-	char	*enval;
-	char	*res;
-	int		i;
-
-	i = 1;
-	res = NULL;
-	split = ft_split(str, '$');
-	if (str[0] == '$')
-		res = getenv(split[0]);
-	if (res == NULL)
-		res = split[0];
-	while (split[i])
-	{
-		enval = getenv(split[i]);
-		if (enval != NULL)
-			ft_strlcat(res, enval, strlen(res) + strlen(enval) + 1);
-		i++;
-	}
-	return (ft_strlen(str) - (int)ft_strlen(res));
-}
-*/
 
 int	quote_skip_strlen(char *arguments, int *quote, int *param)
 {
@@ -162,6 +114,7 @@ void	extra_strcpy(t_cmd_args *args, t_astree *ast_node)
 	int			quote;
 	int			param;
 	int			len;
+	char		*expand;
 
 	i = 0;
 	quote = 0;
@@ -169,11 +122,12 @@ void	extra_strcpy(t_cmd_args *args, t_astree *ast_node)
 			|| ast_node->type == NODE_CMDPATH))
 	{
 		len = quote_skip_strlen(ast_node->szData, &quote, &param);
-		/////////////// 環境変数を展開する
-		if (isenval(ast_node->szData))
+		if (param != single_q && isenval(ast_node->szData))
 		{
-			expand_enval(&(ast_node->szData), ast_node->szData);
-			len = quote_skip_strlen(ast_node->szData, &quote, &param);
+			expand = expand_enval(ast_node->szData);
+			len = quote_skip_strlen(expand, &quote, &param);
+			free(ast_node->szData);
+			ast_node->szData = expand;
 		}
 		args->cmdpath[i] = (char *)malloc(len + 1);
 		quote_skip_strcpy(args->cmdpath[i], ast_node->szData, param);
