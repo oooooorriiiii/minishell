b256af6144b1c0297be7120a7aba53509ed1cc11
SoichiroSugimoto <fcsoichi1998@gmail.com>
2022-05-15 09:40:57 +0900

    fix ft_function

--- srcs/execute/execute_command_struct.c
+++ srcs/execute/execute_command_struct.c
@@ -6,7 +6,7 @@
 /*   By: sosugimo <sosugimo@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 12:38:26 by sosugimo          #+#    #+#             */
-/*   Updated: 2022/05/14 11:25:52 by sosugimo         ###   ########.fr       */
+/*   Updated: 2022/05/15 09:37:22 by sosugimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,30 +29,30 @@ void	execute_external_cmd(t_cmd_args *args)
 	env_strs = gen_env_str(g_minishell.env);
 	path = add_path(args);
 	ret = execve(path, args->cmdpath, env_strs);
-	// if (ret < 0)
-	// 	msh_fatal("execve error: ");
+	if (ret < 0)
+		msh_fatal("execve error: ");
 	free_str(&path);
 	free_str_arr(&env_strs);
 }
 
 void	execute_command_struct2(t_cmd_args *args)
 {
-	if (strcmp(args->cmdpath[0], "export") == 0)
+	if (ft_strcmp(args->cmdpath[0], "export") == 0)
 	{
 		execute_export(args);
 		return ;
 	}
-	else if (strcmp(args->cmdpath[0], "unset") == 0)
+	else if (ft_strcmp(args->cmdpath[0], "unset") == 0)
 	{
 		execute_unset(args);
 		return ;
 	}
-	else if (strcmp(args->cmdpath[0], "env") == 0)
+	else if (ft_strcmp(args->cmdpath[0], "env") == 0)
 	{
 		execute_env(args);
 		return ;
 	}
-	else if (strcmp(args->cmdpath[0], "exit") == 0)
+	else if (ft_strcmp(args->cmdpath[0], "exit") == 0)
 	{
 		execute_exit(args);
 		return ;
@@ -66,22 +66,22 @@ void	execute_command_struct(t_cmd_args *args)
 		return ;
 	else if (args->cmdpath[0] == NULL)
 		execute_external_cmd(args);
-	else if (strcmp(args->cmdpath[0], "echo") == 0)
+	else if (ft_strcmp(args->cmdpath[0], "echo") == 0)
 	{
 		execute_echo(args);
 		return ;
 	}
-	else if (strcmp(args->cmdpath[0], "cd") == 0)
+	else if (ft_strcmp(args->cmdpath[0], "cd") == 0)
 	{
 		execute_cd(args);
 		return ;
 	}
-	else if (strcmp(args->cmdpath[0], "pwd") == 0)
+	else if (ft_strcmp(args->cmdpath[0], "pwd") == 0)
 		return (execute_pwd(args));
-	else if (!strcmp(args->cmdpath[0], "export")
-		|| !strcmp(args->cmdpath[0], "unset")
-		|| !strcmp(args->cmdpath[0], "env")
-		|| !strcmp(args->cmdpath[0], "exit"))
+	else if (!ft_strcmp(args->cmdpath[0], "export")
+		|| !ft_strcmp(args->cmdpath[0], "unset")
+		|| !ft_strcmp(args->cmdpath[0], "env")
+		|| !ft_strcmp(args->cmdpath[0], "exit"))
 		execute_command_struct2(args);
 	else
 		execute_external_cmd(args);
