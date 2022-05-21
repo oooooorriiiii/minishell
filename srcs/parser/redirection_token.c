ce195aa2c43cffa6cbcc7809967763eaec6a3cb4
ymori <yuu.mori06@gmail.com>
2022-05-21 21:01:40 +0900

    add: redirection

--- srcs/parser/redirection_token.c
+++ srcs/parser/redirection_token.c
@@ -9,6 +9,8 @@ t_astree	*RIDIRECTION1(t_token_list **curtok)
 	t_astree	*result;
 	char		*filename;
 
+	if (!term(TOKEN, &filename, curtok))
+		return (NULL);
 	result = malloc(sizeof(*result));
 	parse_malloc_errordeal(result, NULL);
 	astreeset_type(result, NODE_REDIRECT_IN);
