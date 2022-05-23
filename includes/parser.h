0b01a376aa2dba714dcd5cb412f1df451bfccb23
ymori <yuu.mori06@gmail.com>
2022-05-24 02:53:35 +0900

    fix: REDIRECTION_LIST

--- includes/parser.h
+++ includes/parser.h
@@ -23,16 +23,16 @@
 
 typedef enum e_node_type
 {
-	NODE_PIPE			= (1 << 0),
-	NODE_BCKGRND		= (1 << 1),
-	NODE_REDIRECTION	= (1 << 2),
-	NODE_REDIRECT_IN	= (1 << 3),
-	NODE_REDIRECT_OUT	= (1 << 4),
-	NODE_REDIRECT_D_IN	= (1 << 5),
-	NODE_REDIRECT_D_OUT = (1 << 6),
-	NODE_CMDPATH		= (1 << 7),
-	NODE_ARGUMENT		= (1 << 8),
-	NODE_DATA			= (1 << 9),
+	NODE_PIPE				= (1 << 0),
+	NODE_REDIRECTION_LIST	= (1 << 1),
+	NODE_REDIRECTION		= (1 << 2),
+	NODE_REDIRECT_IN		= (1 << 3),
+	NODE_REDIRECT_OUT		= (1 << 4),
+	NODE_REDIRECT_D_IN		= (1 << 5),
+	NODE_REDIRECT_D_OUT 	= (1 << 6),
+	NODE_CMDPATH			= (1 << 7),
+	NODE_ARGUMENT			= (1 << 8),
+	NODE_DATA				= (1 << 9),
 }		t_node_type;
 
 typedef struct astree
--- srcs/parser/redirection.c
+++ srcs/parser/redirection.c
@@ -1,11 +1,15 @@
 #include "../includes/parser.h"
 
-/*
- * <redirection> ::= '>' <word>   ->REDIRECTION1
- *                 | '<' <word>   ->REDIRECTION2
- *                 | '>>' <word>  ->REDIRECTION11
- *                 | '<<' <word>  ->REDIRECTION22
- */
+ /**
+  *
+  * @param curtok
+  * @return
+  *
+  * <redirection> ::= '>' <word>   ->REDIRECTION1
+  *                 | '<' <word>   ->REDIRECTION2
+  *                 | '>>' <word>  ->REDIRECTION11
+  *                 | '<<' <word>  ->REDIRECTION22
+  */
 t_astree	*REDIRECTION(t_token_list **curtok)
 {
 	t_token_list		*save;
@@ -41,36 +45,19 @@ t_astree	*REDIRECTION(t_token_list **curtok)
 t_astree	*REDIRECTION_LIST1(t_token_list **curtok)
 {
 	t_astree	*redirectionNode;
+	t_astree	*redirectionListNode;
+	t_astree	*result;
 
 	redirectionNode = REDIRECTION(curtok);
 	if (redirectionNode == NULL)
 		return (NULL);
-	return (REDIRECTION_LIST(curtok));
-}
-
-/**
- *
- * @param curtok
- * @return
- *
- * <redirection>
- */
-t_astree	*REDIRECTION_LIST2(t_token_list **curtok)
-{
-	return (REDIRECTION(curtok));
-}
-
-/**
- *
- * @param curtok
- * @return
- *
- * <EMPTY>
- */
-t_astree	*REDIRECTION_LIST3(t_token_list **curtok)
-{
-	(void)curtok;
-	return (NULL);
+	redirectionListNode = REDIRECTION_LIST(curtok);
+	result = malloc(sizeof(t_astree));
+	parse_malloc_errordeal(result, NULL);
+	astreeset_type(result, NODE_REDIRECTION_LIST);
+	astreeset_data(result, NULL);
+	astree_attach(result, redirectionNode, redirectionListNode);
+	return (result);
 }
 
 /**
@@ -79,22 +66,8 @@ t_astree	*REDIRECTION_LIST3(t_token_list **curtok)
  * @return
  *
  * <redirection list> ::= <redirection> <redirection list> -> REDIRECTION_LIST1
- *                      | <redirection>                    -> REDIRECTION_LIST2
- *                      | <EMPTY>                          -> REDIRECTION_LIST3
  */
 t_astree	*REDIRECTION_LIST(t_token_list **curtok)
 {
-	t_token_list	*save;
-	t_astree		*node;
-
-	save = *curtok;
-	*curtok = save;
-	node = REDIRECTION_LIST1(curtok);
-	if (node != NULL)
-		return (node);
-	*curtok = save;
-	node = REDIRECTION_LIST2(curtok);
-	if (node != NULL)
-		return (node);
-	return (REDIRECTION_LIST3(curtok));
+	return (REDIRECTION_LIST1(curtok));
 }
