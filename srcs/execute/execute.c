c9f548fe691dbf1d40b42d0ec8c0275abc33d77f
ymori <yuu.mori06@gmail.com>
2022-05-28 18:58:56 +0900

    delete execute_pipeline
    for norm

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -18,36 +18,6 @@ void	set_pipefd(t_cmd_args *args, bool stdin_pipe, bool stdout_pipe)
 	args->stdout_pipe = stdout_pipe;
 }
 
-//void	execute_pipeline(t_astree *t, t_cmd_args *args)
-//{
-//	t_astree	*jobNode;
-//	int			file_desc[2];
-//
-//	pipe(file_desc);
-//	args->pipe_write = file_desc[1];
-//	args->pipe_read = file_desc[0];
-//	set_pipefd(args, false, true);
-//	execute_command(t->left, args);
-//	jobNode = t->right;
-//	while (jobNode != NULL && jobNode->type == NODE_PIPE)
-//	{
-//		close(args->pipe_write);
-//		pipe(file_desc);
-//		args->pipe_write = file_desc[1];
-//		set_pipefd(args, true, true);
-//		execute_command(jobNode->left, args);
-//		close(args->pipe_read);
-//		args->pipe_read = file_desc[0];
-//		jobNode = jobNode->right;
-//	}
-//	args->pipe_read = file_desc[0];
-//	close(args->pipe_write);
-//	set_pipefd(args, true, false);
-//	execute_command(jobNode, args);
-//	close(args->pipe_read);
-//}
-
-
 void	execute_cmdline(t_astree *cmdline, t_cmd_args *args, int *status)
 {
 	if (cmdline == NULL)
